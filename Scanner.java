6e5b2748a2f02af0d9edbef2c2c3e883905d0357
2len4ik <146033852+2len4ik@users.noreply.github.com>
2023-09-26 20:30:06 +0000

    Convertor

---
+++ Scanner.java
@@ -0,0 +1,16 @@
+import java.io.InputStream;
+
+public class Scanner {
+
+    public Scanner(InputStream in) {
+    }
+
+    public double nextDouble() {
+        return 0;
+    }
+
+    public int nextInt() {
+        return 0;
+    }
+
+}
